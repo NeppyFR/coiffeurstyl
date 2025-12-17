522f3c08bdbe9c150fa262aa371762acbddc203f
NeppyFR <singh-angad@hotmail.com>
2025-12-17 02:19:32 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Replace 'coiffeurstyl' with your exact repository name if it differs
-  base: "/coiffeurstyl/",
+  // Changed to relative path so it works on any repo name
+  base: './',
 });
